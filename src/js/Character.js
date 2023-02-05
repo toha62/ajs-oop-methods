2f6807b40a47baa8b6f1e8134da9c3a39f806a45
Anton Trifonov <toha62@inbox.ru>
2023-02-05 23:16:37 +0300

    add tests

--- src/js/Character.js
+++ src/js/Character.js
@@ -11,4 +11,22 @@ export default class Character {
     this.health = 100;
     this.level = 1;
   }
+
+  levelUp() {
+    if (!this.health) {
+      throw new Error('You can`t level up this character, because he is dead');
+    }
+    this.level += 1;
+    this.attack = Math.round(this.attack * 1.2);
+    this.defence = Math.round(this.defence * 1.2);
+    this.health = 100;
+  }
+
+  damage(points) {
+    if (!this.health) {
+      throw new Error('You can`t damage this character, because he is dead');
+    }
+    const health = this.health - Math.round(points * (1 - this.defence / 100));
+    this.health = health > 0 ? health : 0;
+  }
 }
--- src/js/__tests__/bowerman.test.js
+++ src/js/__tests__/bowerman.test.js
@@ -37,3 +37,46 @@ test.each([
     new Bowerman(name);
   }).toThrow();
 });
+
+test('should throw on levelUp() when health = 0', () => {
+  expect(() => {
+    const bowerman = new Bowerman('Robin Hood');
+    bowerman.health = 0;
+    bowerman.levelUp();
+  }).toThrow();
+});
+
+test('should throw on damage() when health = 0', () => {
+  expect(() => {
+    const bowerman = new Bowerman('Robin Hood');
+    bowerman.health = 0;
+    bowerman.damage(10);
+  }).toThrow();
+});
+
+test('should level up, increase attack, defence by 20% and icrease health to 100', () => {
+  const bowerman = new Bowerman('Robin Hood');
+  bowerman.health = 10;
+  bowerman.level = 2;
+  bowerman.attack = 20;
+  bowerman.defence = 30;
+  bowerman.levelUp();
+
+  expect(bowerman.health).toBe(100);
+  expect(bowerman.level).toBe(3);
+  expect(bowerman.attack).toBe(24);
+  expect(bowerman.defence).toBe(36);
+});
+
+test.each([
+  [80, 20, 65],
+  [10, 20, 0],
+])('should decrease health=%n by %n points depending on the defence level and got health=%n',
+  (health, points, result) => {
+    const bowerman = new Bowerman('Robin Hood');
+    bowerman.health = health;
+    bowerman.defence = 25;
+    bowerman.damage(points);
+
+    expect(bowerman.health).toBe(result);
+  });
