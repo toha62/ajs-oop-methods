1a79e9582284f97a9a5823438cd41e8454acfa65
Anton Trifonov <toha62@inbox.ru>
2023-02-06 22:32:50 +0300

    add character test

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,7 +1,18 @@
 export default class Character {
   constructor(name, type) {
+    const charactersType = [
+      'Bowerman',
+      'Swordsman',
+      'Magician',
+      'Daemon',
+      'Undead',
+      'Zombie',
+    ];
     if (!(typeof (name) === 'string')) {
       throw new Error('"name" is not a string');
+    }
+    if (!charactersType.includes(type)) {
+      throw new Error('type of character not correct');
     }
     if (name.length < 2 || name.length > 10) {
       throw new Error('Length of "name" does not match expected value: from 2 to 10 symbols');
